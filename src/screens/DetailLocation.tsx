6c37579ee417f94d423422a4d092617525530f06
Hyeonmeoung Joo <wugusaud77@icloud.com>
2023-07-21 01:43:57 +0900

    fix:callback issue

--- src/screens/DetailLocation.tsx
+++ src/screens/DetailLocation.tsx
@@ -35,17 +35,16 @@ const DetailLocation = ({navigation}) => {
               <Typography color="gray700" size={22} weight={600}>
                 햄보꾸 헬스장
               </Typography>
-              {/* <HStack spacing={4} center> */}
-              {/*  <StarIcon width={16} height={16} /> */}
-              {/*  <StarIcon width={16} height={16} /> */}
-              {/*  <StarIcon width={16} height={16} /> */}
-              {/*  <StarIcon width={16} height={16} /> */}
-              {/*  <StarIcon width={16} height={16} /> */}
-
-              {/*  <Typography color="yellow500" size={12} weight={500}> */}
-              {/*    5.0 */}
-              {/*  </Typography> */}
-              {/* </HStack> */}
+              <StarContainer>
+                <StarIcon width={20} height={20} />
+                <StarIcon width={20} height={20} />
+                <StarIcon width={20} height={20} />
+                <StarIcon width={20} height={20} />
+                <StarIcon width={20} height={20} />
+                <Typography color="yellow500" size={12} weight={500}>
+                  5.0
+                </Typography>
+              </StarContainer>
             </HealthInfoTop>
             <HeatlhTagList>
               <LocationTag>
@@ -92,10 +91,119 @@ const DetailLocation = ({navigation}) => {
             </SB>
           </VStack>
         </Layout2>
+        <LayoutDivider />
+        <Layout3>
+          <Typography color="gray700" size={16} weight={500}>
+            소개
+          </Typography>
+          <Typography color="gray600" size={14} weight={500}>
+            흥미로운 운동 환경과 다양한 프로그램으로 건강한 라이프스타일을
+            만들어드립니다. 전문 트레이너들과 함께 목표를 이루어내는 동기부여를
+            제공합니다. 언제든지 방문하시어 편안한 분위기에서 자유롭게 운동을
+            즐기세요. 우리 헬스장에서 건강과 즐거움을 만나보세요! 함께라면 더
+            나은 자신을 발견할 수 있습니다.
+          </Typography>
+        </Layout3>
+        <LayoutDivider />
+        <Layout3>
+          <Typography color="gray700" size={16} weight={500}>
+            트레이너 소개
+          </Typography>
+          <VStack spacing={8} center>
+            <TrainerProfile
+              name="김종국"
+              description="다양한 운동 경험과 자격증을 바탕으로 개인 맞춤형 운동 계획을 제공합니다. 건강과 목표 달성을 위해 함께 노력하겠습니다!"
+            />
+            <TrainerProfile
+              name="김종국"
+              description="다양한 운동 경험과 자격증을 바탕으로 개인 맞춤형 운동 계획을 제공합니다. 건강과 목표 달성을 위해 함께 노력하겠습니다!"
+            />
+            <TrainerProfile
+              name="김종국"
+              description="다양한 운동 경험과 자격증을 바탕으로 개인 맞춤형 운동 계획을 제공합니다. 건강과 목표 달성을 위해 함께 노력하겠습니다!"
+            />
+            <TrainerProfile
+              name="김종국"
+              description="다양한 운동 경험과 자격증을 바탕으로 개인 맞춤형 운동 계획을 제공합니다. 건강과 목표 달성을 위해 함께 노력하겠습니다!"
+            />
+            <TrainerProfile
+              name="김종국"
+              description="다양한 운동 경험과 자격증을 바탕으로 개인 맞춤형 운동 계획을 제공합니다. 건강과 목표 달성을 위해 함께 노력하겠습니다!"
+            />
+            <TrainerProfile
+              name="김종국"
+              description="다양한 운동 경험과 자격증을 바탕으로 개인 맞춤형 운동 계획을 제공합니다. 건강과 목표 달성을 위해 함께 노력하겠습니다!"
+            />
+          </VStack>
+        </Layout3>
       </ScrollView>
     </SafeAreaView>
   );
 };
+
+const TrainerProfile = ({name, description}) => {
+  return (
+    <TrainerProfileContainer>
+      <TrainerProfileImage />
+      <TrainerInfo>
+        <Typography color="gray700" size={16} weight={600}>
+          김종국
+        </Typography>
+        <Typography color="gray400" size={12} weight={500}>
+          {description}
+        </Typography>
+      </TrainerInfo>
+    </TrainerProfileContainer>
+  );
+};
+
+const TrainerInfo = styled.View`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  width: 80%;
+`;
+
+const TrainerProfileImage = styled.View`
+  width: 44px;
+  height: 44px;
+  border-radius: 22px;
+  background-color: #f1f2f3;
+`;
+
+const TrainerProfileContainer = styled.View`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  gap: 12px;
+  padding: 16px;
+  background: #fafbfc;
+`;
+
+const Layout3 = styled.View`
+  width: 100%;
+  padding: 0 16px;
+  display: flex;
+  flex-direction: column;
+  margin-top: 16px;
+  gap: 10px;
+`;
+
+const LayoutDivider = styled.View`
+  margin-top: 20px;
+  width: 100%;
+  height: 8px;
+  background-color: #f1f2f3;
+`;
+
+const StarContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 2px;
+  margin-top: 8px;
+`;
 
 const SB = styled.View`
   width: 100%;
@@ -117,7 +225,7 @@ const Layout2 = styled.View`
 const LocationTag = styled.View`
   display: inline-flex;
   padding: 0 9px;
-  height: 24px;
+  height: 28px;
   align-items: center;
   flex-direction: column;
   justify-content: center;
--- src/screens/Measure.tsx
+++ src/screens/Measure.tsx
@@ -9,9 +9,8 @@ import {
 } from '@components/AppBar';
 import CTAButton from '@components/CTAButton';
 import Banner from '@components/Banner';
+import Go from '@assets/icons/go';
 import Typography from '../components/Typography';
-import Banner from "@components/Banner";
-import Go from '@assets/icons/go'
 
 const Measure = ({navigation}) => {
   const [timer, setTimer] = useState([0, 0, 0]);
@@ -38,25 +37,26 @@ const Measure = ({navigation}) => {
         flex: 1,
         backgroundColor: '#fff',
       }}>
-      <AppBarWithTextAndLeft
-        text="달리기 운동"
-      />
-        <Tp>
-            <Typography color='gray700' size={16} weight={500}>현재 운동 시간</Typography>
-            <Timer>
-                <Typography color="gray700" size={32} weight={600}>
-                    {`${String(timer[0]).padStart(2, '0')}:${String(timer[1]).padStart(2, '0')}:${String(
-                        timer[2]
-                    ).padStart(2, '0')}`}
-                </Typography>
-                <Go width={50}/>
-            </Timer>
-        </Tp>
-        <Parent>
-            <Banner/>
-            <Banner/>
-            <Banner/>
-        </Parent>
+      <AppBarWithTextAndLeft text="달리기 운동" />
+      <Tp>
+        <Typography color="gray700" size={16} weight={500}>
+          현재 운동 시간
+        </Typography>
+        <Timer>
+          <Typography color="gray700" size={32} weight={600}>
+            {`${String(timer[0]).padStart(2, '0')}:${String(timer[1]).padStart(
+              2,
+              '0',
+            )}:${String(timer[2]).padStart(2, '0')}`}
+          </Typography>
+          <Go width={50} />
+        </Timer>
+      </Tp>
+      <Parent>
+        <Banner />
+        <Banner />
+        <Banner />
+      </Parent>
       <AppBarWithTextCenter title="달리기 운동" />
       <Timer>
         <Typography color="gray700" size={40} weight={600}>
@@ -94,10 +94,10 @@ const BottomButton = styled.View`
 
 const Tp = styled.View`
   margin-top: 100px;
-    width: 100%;
+  width: 100%;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Parent = styled.View`
   position: absolute;
